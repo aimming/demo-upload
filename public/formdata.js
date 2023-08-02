afad7427c520fbf4b31af77b32429c5097417623
aimming <ainaprogramming@gmail.com>
2023-08-02 11:21:45 +0200

    validacionarchivos

--- public/formdata.js
+++ public/formdata.js
@@ -2,11 +2,32 @@ $(document).ready(function() {
   $("#uploadForm").submit(function(e) {
     e.preventDefault();
     var formData = new FormData();
-    var fileInput = document.getElementById("fileInput");
+    var fileInput = document.getElementById("archivo");
+
+    // Validación de archivos
+    var tiposPermitidos = [".pdf", ".doc", ".docx"];
+    var tamañoMaximoMB = 10; // Tamaño máximo permitido en MB
 
     if (fileInput.files.length > 0) {
-      formData.append("file", fileInput.files[0]);
+      var archivo = fileInput.files[0];
+      var tipoArchivo = '.' + archivo.name.split('.').pop().toLowerCase();
 
+      if (!tiposPermitidos.includes(tipoArchivo)) {
+        $("#message").text("Tipo de archivo no permitido. Los formatos permitidos son: " + tiposPermitidos.join(", "));
+        fileInput.value = ""; // Limpiar el input de archivo
+        return;
+      }
+
+      var tamañoMB = archivo.size / 1024 / 1024;
+      if (tamañoMB > tamañoMaximoMB) {
+        $("#message").text("El tamaño del archivo excede el límite permitido de " + tamañoMaximoMB + " MB.");
+        fileInput.value = ""; // Limpiar el input de archivo
+        return;
+      }
+
+      formData.append("file", archivo);
+
+      // Si pasa la validación, realizar la carga del archivo
       $.ajax({
         type: "POST",
         url: "../upload.php", // Ajusta esta URL para que apunte correctamente a tu archivo "upload.php" en el servidor.
